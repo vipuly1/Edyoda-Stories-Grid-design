693b423dd5ac809e895e11ca1a253132cad37f7f
vipuly1 <56862842+vipuly1@users.noreply.github.com>
2021-10-17 23:05:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import Admin from "./Admin";
+import Grid from "./Grid"
+import Header from "./Header";
+import Footer from "./Footer";
+import "./Grid.css"
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <Header/>
+      <Grid/>
+      <Footer/>
+
+
+
+      {/* <Admin></Admin> */}
+    </div>
+  );
+}
+
+export default App;
